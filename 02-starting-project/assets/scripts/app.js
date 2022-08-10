fa9846fb1f0066b6e0bbc80bfb36681b891aba1e
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-08-10 17:26:18 +0200

    lesson 24

--- 02-starting-project/assets/scripts/app.js
+++ 02-starting-project/assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('this works')
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription);
