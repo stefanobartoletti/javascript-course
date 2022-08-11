4602f652a45009915cdeca6dd58057db00f4f76f
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-08-11 16:30:01 +0200

    add template literal

--- 02-starting-project/assets/scripts/app.js
+++ 02-starting-project/assets/scripts/app.js
@@ -3,6 +3,12 @@ let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
+/**
+Template literal, equivalent to
 let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+*/
+
+let calculationDescription = `(${ defaultResult } + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
