78603c7816fbd1e85009c1b940c8163dd2e56cbc
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-08-12 17:28:57 +0200

    split code in multiple functions

--- module-02/lessons-01-starting-project/assets/scripts/app.js
+++ module-02/lessons-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserInput() {
+    return parseInt(userInput.value);
+}
+
 function add() {
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    const enteredNumber = getUserInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add)
