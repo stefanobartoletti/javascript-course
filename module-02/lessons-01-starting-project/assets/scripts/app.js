f37f74c05fa56bb6ce528b45b5ac7caa6b3fa72f
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-08-12 17:38:18 +0200

    add operator functions

--- module-02/lessons-01-starting-project/assets/scripts/app.js
+++ module-02/lessons-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
+// --- Variables ---
+
 const defaultResult = 0;
 let currentResult = defaultResult;
+
+// --- Helper functions ---
 
 function getUserInput() {
     return parseInt(userInput.value);
 }
+
+// --- Math functions ---
 
 function add() {
     const enteredNumber = getUserInput();
@@ -12,6 +18,32 @@ function add() {
     outputResult(currentResult, calcDescription);
 }
 
+function subtract() {
+    const enteredNumber = getUserInput();
+    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+    const enteredNumber = getUserInput();
+    const calcDescription = `${currentResult} * ${enteredNumber}`;
+    currentResult = currentResult * enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+    const enteredNumber = getUserInput();
+    const calcDescription = `${currentResult} / ${enteredNumber}`;
+    currentResult = currentResult / enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+// --- Event listeners ---
+
 addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
 
 
