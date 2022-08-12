39c92410acf1d8f0e782758ae254c913f844744e
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-08-12 16:49:41 +0200

    exercise 2

--- module-02/exercise-02/assignment.js
+++ module-02/exercise-02/assignment.js
@@ -1,1 +1,21 @@
 const task3Element = document.getElementById('task-3');
+
+
+function createAlert() {
+    alert('"createAlert" has run');
+}
+
+function alertName(name) {
+    alert(`'alertName' has run: ${name}`);
+}
+
+function concatenateStrings(str1, str2, str3) {
+    alert(`'concatenateStrings' has run: ${str1} | ${str2} | ${str3}`);
+}
+
+createAlert();
+alertName('stefano');
+
+task3Element.addEventListener('click', createAlert)
+
+concatenateStrings('qui', 'quo', 'qua')
