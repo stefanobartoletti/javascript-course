277965d4964164a59808e4d2a6e43612f2697356
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-08-12 14:46:40 +0200

    functions

--- module-02/lessons-01-starting-project/assets/scripts/app.js
+++ module-02/lessons-01-starting-project/assets/scripts/app.js
@@ -1,21 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-currentResult = add(1, 2);
+addBtn.addEventListener('click', add)
 
-/**
-Template literal, equivalent to
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-*/
 
-let calculationDescription = `(${ defaultResult } + 10) * 3 / 2 - 1`;
-
-console.log( calculationDescription)
-
-outputResult(currentResult, calculationDescription);
