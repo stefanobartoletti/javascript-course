8c06a8637eef7890aba6721724298f7976171f1f
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-11-02 12:32:31 +0000

    add reset function

--- module-04/lessons-02-monster-killer-starting-project/assets/scripts/app.js
+++ module-04/lessons-02-monster-killer-starting-project/assets/scripts/app.js
@@ -10,6 +10,12 @@ let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
+
 function endRound() {
     const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -25,10 +31,17 @@ function endRound() {
 
     if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
         alert('Draw!');
+        // reset();
     } else if (currentMonsterHealth <= 0) {
         alert ('You won!');
+        // reset();
     } else if (currentPlayerHealth <= 0) {
         alert ('You lost!');
+        // reset();
+    }
+
+    if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+        reset();
     }
 }
 
