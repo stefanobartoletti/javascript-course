1c56c517e5569eb007c8aac763e78b916e0aaf30
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-11-02 12:47:23 +0000

    add variables for attack modes

--- module-04/lessons-02-monster-killer-starting-project/assets/scripts/app.js
+++ module-04/lessons-02-monster-killer-starting-project/assets/scripts/app.js
@@ -3,6 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACK = 'ATTACK';
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK'
+
 const enteredValue = prompt('Choose Maximum life', '100');
 
 let chosenMaxLife = parseInt(enteredValue);
@@ -55,9 +58,9 @@ function endRound() {
 
 function attackMonster(mode) {
     let maxDamage;
-    if (mode === 'ATTACK') {
+    if (mode === MODE_ATTACK) {
         maxDamage = ATTACK_VALUE;
-    } else if (mode === 'STRONG_ATTACK') {
+    } else if (mode === MODE_STRONG_ATTACK) {
         maxDamage = STRONG_ATTACK_VALUE;
     }
 
@@ -69,11 +72,11 @@ function attackMonster(mode) {
 
 
 function attackHandler() {
-    attackMonster('ATTACK');
+    attackMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler() {
-    attackMonster('STRONG_ATTACK');
+    attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
